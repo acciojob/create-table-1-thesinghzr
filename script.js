00b6453c6d5daebfa872a8ed2c1586156d16d8a4
Suraj Singh <yousurajsingh@gmail.com>
2025-03-23 20:56:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
-var button = document.getElementById("insert");
-
 function insert_Row() {
-    var sampleTable = document.getElementById("sampleTable");
+	let table = document.getElementById("sampleTable");
 
-    var newRow = document.createElement("tr");
+	let newRow = table.insertRow(0);
+	
+	let cell1 = newRow.insertCell(0);
+    let cell2 = newRow.insertCell(1);
 
-    var cell1 = document.createElement("td");
+   
     cell1.textContent = "New Cell1";
-
-    var cell2 = document.createElement("td");
-    cell2.textContent = "New Cell2"; 
-
-    newRow.appendChild(cell1);
-    newRow.appendChild(cell2);
-
-    sampleTable.insertBefore(newRow, sampleTable.firstChild);
+    cell2.textContent = "New Cell2";
+  
 }
-
